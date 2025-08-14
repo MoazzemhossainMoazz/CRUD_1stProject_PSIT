9f0bef00df6abd9b39b5e99125eef0dc27ade83b
unknown <moazzemh222@gmail.com>
2025-08-14 23:57:56 +0600

    Add Edit Controller

--- FirstProject/Controllers/EmployeeController.cs
+++ FirstProject/Controllers/EmployeeController.cs
@@ -65,7 +65,59 @@ namespace FirstProject.Controllers
             return View(employee);
         }
 
+        public ActionResult Edit(int id)
+        {
+            var model = _dbContext.Employees.Find(id);
 
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EmployeeModels employee)
+        {
+            if (ModelState.IsValid)
+            {
+                if (employee.Picture != null)
+                {
+                    string fileName = Path.GetFileName(employee.Picture.FileName);
+                    string extension = Path.GetExtension(fileName);
+                    string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Pictures", employee.Name + extension);
+
+                    if (extension != ".jpg" && extension != ".png" && extension != "jpeg")
+                    {
+                        ModelState.AddModelError("Picture", "Only .jpg, .png, .jpeg file are allowed");
+                        return View(employee);
+                    }
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        employee.Picture.CopyTo(stream);
+                    }
+                    employee.PicturePath = "/Pictures/" + employee.Name + extension;
+
+                }
+
+                else
+                {
+                    employee.PicturePath = employee.PicturePath;
+                }
+
+                _dbContext.Employees.Update(employee);
+                if (_dbContext.SaveChanges() > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    var message = string.Join(" | ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage
+                    ));
+                    ModelState.AddModelError(" ", message);
+                    return View(employee);
+                }
+            }
+
+            return View(employee);
+        }
 
 
     }
